4aefae06ba134c53e88c5fb455622866ac15c99f
Ed <67863060+ens100@users.noreply.github.com>
2020-10-11 18:48:50 +0100

    Update Palindrome.py

--- Code/Palindrome.py
+++ Code/Palindrome.py
@@ -1,7 +1,8 @@
 def main():
     word = input("What is your word? ")
-    w = word[::-1]
-    if word == w:
+    string = word.replace(" ", "")
+    w = string[::-1]
+    if string == w:
         print("Great, {} is a Palindrome".format(word))
     else:
         print("Sorry, {} is not a Palindrome here".format(word))
