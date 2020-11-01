98aecd8a1a7dab87e465ee2a7280b105bb3ce0b6
Ed <67863060+ens100@users.noreply.github.com>
2020-11-01 09:52:17 +0000

    Update rock_paper_scissors.py
    Update to basic game

--- Code/rock_paper_scissors.py
+++ Code/rock_paper_scissors.py
@@ -1,12 +1,50 @@
-import random
+"""Rock, Paper, Scissors game - Comp vs User"""
 
-def main():
-    options = ["rock", "paper", "scissors"]
-    comp = random.choice(options)
-    choice = int(input("1 for Rock, 2 for Paper, 3 for Scissors? "))
-    user = options[choice-1]
-    print("Computer: ", comp)
-    print("User: ", user)
+from random import randint
 
+user_score = 0
+comp_score = 0
+count_games = 1
 
-main()
+while True: #Keep the game running TODO: give the user an easy out.
+
+    choice = {1 : "Rock", 2: "Paper", 3 : "Scissors"}
+
+    # User selection and randomise the comp selection:
+    user = int(input("What is your weapon? (1 : Rock, 2: Paper, 3 : Scissors) ")) # TODO add in input control
+    user_choice = (choice[user])
+    comp_choice = (choice[randint(1,3)])
+
+    print("---User: " + user_choice + " vs Comp: " + comp_choice)  
+    
+    # Go through the cycle of win, lose or draw:
+    if user_choice == "Rock" and comp_choice == "Scissors":
+        print("---WIN---")
+        user_score += 1
+    elif user_choice == "Rock" and comp_choice == "Paper":
+        print("---LOSE---")
+        comp_score += 1
+    elif user_choice == "Paper" and comp_choice == "Rock":
+        print("---WIN---")        
+        user_score += 1
+    elif user_choice == "Paper" and comp_choice == "Scissors":
+        print("---LOSE---")
+        comp_score += 1
+    elif user_choice == "Scissors" and comp_choice == "Paper":
+        print("---WIN---")        
+        user_score += 1
+    elif user_choice == "Scissors" and comp_choice == "Rock":
+        print("---LOSE---")
+        comp_score += 1
+    else:
+        print("---DRAW---")
+    
+    # print out KPI of games (score, count)
+    print("User: " + str(user_score) + " - Comp: " + str(comp_score)) 
+    print("Games: " + str(count_games))
+    
+    count_games += 1
+
+    
+
+    
