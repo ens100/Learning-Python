153611c15139ee51736410dda0404fceef4afaec
Ed <67863060+ens100@users.noreply.github.com>
2021-04-02 10:03:11 +0100

    Update selenium_scrap.py
    refined code to make it work (just about)

--- Code/selenium_scrap.py
+++ Code/selenium_scrap.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import csv
 
 PATH = "/home/ed/Documents/python/chromedriver" # location of the webdriver - amend as requried
 
@@ -14,18 +16,31 @@ member_list = []
 
 # Flow to open page and click on link to extract href
 
-members = driver.find_elements_by_xpath('//*[@class="projectImageOuter"]') # Looking for the required class - the image which on click bring up the info
-
+members = driver.find_elements_by_class_name("projectImage") # Looking for the required class - the image which on click bring up the info
 
 for member in members:
     print(member.text) # to see that loop went to next iteration
-    member.find_element_by_xpath('//*[@class="projectImage"]').click()
+    member.click()
     wait = WebDriverWait(driver, 10)
-    element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'VIEW FULL PROFILE')))
+    element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'VIEW FULL PROFILE'))) #wait for the element to appear before moving on to avoid crash
     links = driver.find_element_by_partial_link_text("VIEW FULL PROFILE")
     href = links.get_attribute("href")
     member_list.append(href)
     member.find_element_by_xpath("/html/body/div[5]/div[1]/button").click()
+    driver.implicitly_wait(4)
 
-    print(member_list)
+print(member_list)
+
+# Loop through the links to extract the sought information
+
+with open("member_data.csv", "a+") as csv_file:
+    writer = csv.writer(csv_file)
+
+    for member in member_list:
+        df = pd.read_html(member)
+        print(df)
+        writer.writerow(df)
+
+#! Issue - does not append as CSV so cannot convert to DF - to review.
+
 driver.quit()
